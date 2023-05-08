8574804ad28e8078a2c6ef8daa7487740d0a0111
reza <rzxsism80@gmail.com>
2023-05-08 12:08:50 +0330

    config module added to load .env file from root directory:

--- packages/auth/src/app/app.module.ts
+++ packages/auth/src/app/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Mongoose } from 'mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(`${process.env.MONGODB_URL}`),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
